f60acd1509aacf6417984cc817659e855cbadc89
imontalvomiguel <ubaldo.im@gmail.com>
2017-12-13 14:57:33 -0600

    Creating a customer in Stripe

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,13 @@
 class SubscriptionsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
 
   end
 
   def create
+    customer = current_user.stripe_customer
+
     render action: :new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def stripe_customer
+    unless stripe_id?
+      stripe_customer = Stripe::Customer.create(email: email)
+      update(stripe_id: stripe_customer.id)
+      return stripe_customer
+    end
+
+    Stripe::Customer.retrieve(stripe_id)
+  end
 end
